915e80508268a57dd5d82b2b072baa91c39279a5
Brodie Brock <brodiebrock@gmail.com>
2019-07-11 09:06:29 -0400

    created a mini movie fan app with routers

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import Home from './Home';
-import About from './About';
+import Movie from './Movie';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-
         <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
+        <Route exact path="/movie/:movieId" component={Movie} />
       </div>
     </Router>
   );
